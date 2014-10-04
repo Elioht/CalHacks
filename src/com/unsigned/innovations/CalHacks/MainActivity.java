19507778a989f480760c19a4d8e9658fcb43521c
Daniel Diaz <diazjfdaniel@gmail.com>
2014-10-04 08:10:26 -0700

    Working on nav bar

---
+++ src/com/unsigned/innovations/CalHacks/MainActivity.java
@@ -0,0 +1,137 @@
+package com.unsigned.innovations.CalHacks;
+
+
+import android.R.layout;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+public class MainActivity extends ActionBarActivity implements OnItemClickListener {
+	
+	private DrawerLayout drawerLayout;
+	private ActionBarDrawerToggle drawerListener;
+	private ListView listView;
+	private String[] options;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		drawerLayout = (DrawerLayout)findViewById(R.id.drawerLayout);
+		listView = (ListView)findViewById(R.id.drawerList);
+		options = getResources().getStringArray(R.array.nav_drawer);
+		drawerListener = new ActionBarDrawerToggle(this, drawerLayout, R.drawable.ic_navigation_drawer, R.string.drawer_open, R.string.drawer_close){
+
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				// TODO Auto-generated method stub
+				super.onDrawerOpened(drawerView);
+				
+			}
+
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				// TODO Auto-generated method stub
+				super.onDrawerClosed(drawerView);
+				
+			}
+			
+		};
+		
+		drawerLayout.setDrawerListener(drawerListener);
+		getActionBar().setHomeButtonEnabled(true);
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		listView.setOnItemClickListener(this);
+	}
+	
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onPostCreate(savedInstanceState);
+		drawerListener.syncState();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		getMenuInflater().inflate(R.menu.splash, menu);
+		return true;
+	}
+	
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		// TODO Auto-generated method stub
+		super.onConfigurationChanged(newConfig);
+		drawerListener.onConfigurationChanged(newConfig);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		if(drawerListener.onOptionsItemSelected(item)){
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+		// TODO Auto-generated method stub
+		//switch statement
+		//launch activity depending onthe positin
+		selectItem(position);
+	}
+	
+	public void selectItem(int position){
+		listView.setItemChecked(position, true);
+		setTitle("Hello world");
+	}
+
+	public void setTitle(String title){
+		getActionBar().setTitle(title);
+	}
+
+}
+
+//fetching string array
+class MyAdapter extends BaseAdapter{
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int arg0) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int arg0, View arg1, ViewGroup arg2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
