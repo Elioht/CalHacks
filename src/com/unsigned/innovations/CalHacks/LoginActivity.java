5b77bd2e7d0418e486cd1c6c87a96e271abe15ff
Hector Sandoval <hesandova@gmail.com>
2014-10-04 20:46:00 -0700

    Both fragments appear on the screen

--- src/com/unsigned/innovations/CalHacks/LoginActivity.java
+++ src/com/unsigned/innovations/CalHacks/LoginActivity.java
@@ -1,26 +1,45 @@
 package com.unsigned.innovations.CalHacks;
 
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.os.Bundle;
 
-public class LoginActivity extends FragmentActivity {
-		private FacebookFragment facebookFragment;
-		private static final String TAG = "facebookFragment";
-		
-		@Override
-		public void onCreate(Bundle savedInstanceState) {
-		    super.onCreate(savedInstanceState);
+public class LoginActivity extends FragmentActivity implements Communicator {
+	private FacebookFragment facebookFragment;
+	private static final String TAG = "facebookFragment";
 
-		    if (savedInstanceState == null) {
-		        // Add the fragment on initial activity setup
-		        facebookFragment = new FacebookFragment();
-		        getSupportFragmentManager()
-		        .beginTransaction()
-		        .add(android.R.id.content, facebookFragment)
-		        .commit();
-		    } else {
-		        // Or set the fragment from restored state info
-		        facebookFragment = (FacebookFragment) getSupportFragmentManager().findFragmentById(android.R.id.content);
-		    }
-		}      
-}
+	@Override
+
+
+		    protected void onCreate(Bundle savedInstanceState) {		
+		    			// TODO Auto-generated method stub		
+		    			super.onCreate(savedInstanceState);		
+		    			setContentView(R.layout.activity_login);		
+		    		}		
+		    				
+		    		@Override		
+		    		public boolean onCreateOptionsMenu(Menu menu) {		
+		    			// TODO Auto-generated method stub		
+		    			getMenuInflater().inflate(R.menu.splash, menu);		
+		    			return true;		
+		    		}		
+		    				
+		    		@Override		
+		    		public boolean onOptionsItemSelected(MenuItem item) {		
+		    			// TODO Auto-generated method stub		
+		    			int id = item.getItemId();		
+		    			if(id == R.id.action_settings)		
+		    				return true;		
+		    			return super.onOptionsItemSelected(item);		
+		    		}		
+		    			
+		    		@Override		
+		    		public void response(boolean accepted) {		
+		    			// TODO Auto-generated method stub		
+		    			android.app.FragmentManager manager= getFragmentManager();
+		    		}
+	}
+
+
